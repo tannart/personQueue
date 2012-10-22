185f3686017eab0f613dd79bb30d83f0f1451dc5
charliembrodie <cbrodi01@cspc060.dcs.bbk.ac.uk>
2012-10-22 14:01:18 +0100

    See Person commit.

---
+++ Supermarket.java
@@ -0,0 +1,10 @@
+public class Supermarket{
+
+    public addPerson(Person thePerson){
+	    thePerson.insert(aPerson);
+	}
+	
+	public servePerson(Person thePerson){
+	    thePerson.retrieve()
+	}
+}
