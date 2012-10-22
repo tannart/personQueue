5cdb783200d3c72c6eb6c216e453a45f44b4fbfc
charliembrodie <cbrodi01@cspc060.dcs.bbk.ac.uk>
2012-10-22 17:04:12 +0100

    I'm not so sure this was as easy as I had suspected.

--- Supermarket.java
+++ Supermarket.java
@@ -1,10 +1,40 @@
 public class Supermarket{
 
-    public addPerson(Person thePerson){
-	    thePerson.insert(aPerson);
-	}
+    public static void main(String[] args){
+	    Person personBegin = new Person(0);
 	
-	public servePerson(Person thePerson){
-	    thePerson.retrieve()
+	    Person firstPerson = new Person(15);
+	    Person secondPerson = new Person(13);
+	    Person thirdPerson = new Person(12);
+	    Person fourthPerson = new Person(19);
+	    Person fifthPerson = new Person(43);
+	    Person sixthPerson = new Person(39);
+	    Person seventhPerson = new Person(11);
+	
+	    personBegin.addPerson(firstPerson);
+	    personBegin.addPerson(secondPerson);
+	    personBegin.addPerson(thirdPerson);
+	    personBegin.addPerson(fourthPerson);
+	    personBegin.addPerson(fifthPerson);
+	    personBegin.addPerson(sixthPerson);
+	    personBegin.addPerson(seventhPerson);
+		
+		personBegin = firstPerson;
+	
+	    System.out.print("There are " + personBegin.getQueueSize());
+		System.out.println(" people in the queue.");
+		
+		
+		personBegin.servePerson();
+		personBegin.servePerson();
+		personBegin.servePerson();
+		
+		while(personBegin != null){
+		    System.out.println(personBegin.getItems());
+			personBegin = personBegin.getNext();
+		}
+	
+	    System.out.print("There are " + personBegin.getQueueSize());
+		System.out.println(" people in the queue.");
 	}
 }
